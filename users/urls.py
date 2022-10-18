4e76e6f323b3a96dbfeae3e7aa0b07fdbdf43449
plebby08 <plebby.weng@gmail.com>
2022-10-18 12:09:17 +0800

    modified:   Pipfile new file:   Template/add.html new file:   Template/detail.html new file:   Template/edit.html new file:   Template/index1.html new file:   Template/partials/_navbar.html new file:   Template/temp1.html new file:   Template/test1.html new file:   main/__pycache__/__init__.cpython-39.pyc new file:   main/__pycache__/settings.cpython-39.pyc new file:   users/__pycache__/__init__.cpython-39.pyc new file:   users/__pycache__/admin.cpython-39.pyc new file:   users/__pycache__/apps.cpython-39.pyc new file:   users/__pycache__/models.cpython-39.pyc renamed:    index.html -> users/index.html new file:   users/static/css/style.css new file:   users/static/image/chuchu.png modified:   users/urls.py modified:   users/views.py

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,12 @@ from django.conf.urls.static import static
 
 app_name = 'users'
 urlpatterns = [
-    path('', views.index, name='index') 
+    path('', views.index, name='index'),
+    path('add', views.add, name='add'),
+    path('search', views.search, name='search'),
+    path('processadd', views.processadd, name='processadd'),
+    path('<int:profile_id>/detail/', views.detail, name='detail'),
+    path('<int:profile_id>/delete/', views.delete, name='delete'),
+    path('<int:profile_id>/edit/', views.edit, name='edit'),
+    path('<int:profile_id>/processedit/', views.processedit, name='processedit'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) + static (settings.STATIC_URL, document_root = settings.STATIC_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,5 +1,99 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from audioop import reverse
+from http.client import HTTPResponse
+from django.shortcuts import render, get_object_or_404, reverse
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .models import User
+from django.core.paginator import Paginator
+from django.db.models import Q 
+from django.db import IntegrityError
+from django.core.exceptions import ObjectDoesNotExist
+
+
+
+
 
 def index(request):
-    return HttpResponse('Hello Shidz!')
+    user_list = User.objects.all().order_by('-id')
+    paginator = Paginator(user_list, 5)
+
+    page_number = request.GET.get('page')
+
+    user_list = paginator.get_page(page_number)
+
+    return render(request, 'Template/index1.html' {'page_obj': user_list})
+
+def search(request):
+    term = request.GET.get('search', '')
+    user_list = User.objects.filter(Q (user_fname__icontains=term) | Q (user_lname__icontains=term)).order_by('-id')
+
+    paginator = Paginator(user_list, 5)
+
+    page_number = request.GET.get('page')
+
+    user_list = paginator.get_page(page_number)
+
+    return render(request, 'Template/index1.html' {'page_obj': user_list})
+
+def add(request):
+    return render(request, 'Template/add.html')
+
+def processadd(request):
+    fname = request.POST.get('fname')
+    lname = request.POST.get('lname')
+    email = request.POST.get('email')
+    position = request.POST.get('position')
+    if request.FILES.get('image'):
+        user_pic = request.FILES.get('image')
+    else:
+        user_pic = 'profile_pic/image.jpg'
+    try:
+        n = User.objects.get(user_email=email)
+        # number already exists
+        return render(request, 'Template/add.html' , {'error message' : "Duplicated email : " + email})
+    except ObjectDoesNotExist
+        user = User.objects.create(user_email=email, user_fname=fname, user_lname=lname, user_position=position, user_image=user_pic)
+        user.save()
+        return HttpResponseRedirect('/users')
+
+def detail(request, profile_id):
+    try:
+        user= User.objects.get(pk=profile_id)
+    except User.DoesNotExist:
+        raise Http404("Profile does not exist")
+    return render(request, 'Template/detail.html', {'user': user})
+
+def delete(request, profile_id):
+    User.objects.filter(id=profile_id).delete()
+    return HttpResponseRedirect('/users')
+
+def edit(request, profile_id):
+    try:
+        user= User.objects.get(pk=profile_id)
+    except User.DoesNotExist:
+        raise Http404("Profile does not exist")
+    return render(request, 'Template/edit.html', {'user': user})
+
+def processedit(request, profile_id):
+    user= get_object_or_404(User, pk=profile_id)
+    profile_pic = request.FILES.get('image')
+    try:
+        fname = request.POST.get('fname')
+        lname = request.POST.get('lname')
+        email = request.POST.get('email')
+        position = request.POST.get('position')
+    except (KeyError, User.DoesNotExist):
+        return render(request, 'Template/detail.html' , {
+            'user': user,
+            'error_message': "Problem updating record",
+        })
+    else:
+        user_profile = User.objects.get(id=profile_id)
+        user_profile.user_fname = fname
+        user_profile.user_lname = lname
+        user_profile.user_email = email
+        user_profile.user_position = position
+        if profile_pic:
+            user_profile.user_image = profile_pic
+        user_profile.save()
+        return HttpResponseRedirect(reverse('users:detail', args=(profile_id,)))
+
